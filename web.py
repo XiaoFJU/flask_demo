b84df9832c42e099e73e3b267aa9bdcaa3431d0d
xiao <freedom85812@gmail.com>
2018-05-11 03:32:13 +0800

    say hello for /user/<name>

--- web.py
+++ web.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/user/<name>')
+def hello_for(name):
+    return 'Hello, ' + name
+
 
 if __name__ == "__main__":
     app.run()
