463691bfff5603ddb434c97c1675fcef2c196a95
xiao <freedom85812@gmail.com>
2018-05-11 19:19:22 +0800

    input post

--- web.py
+++ web.py
@@ -8,9 +8,17 @@ def hello():
     return 'Hello, World!'
 
 
-@app.route('/input',  methods=['GET'])
+@app.route('/input',  methods=['GET', 'POST'])
 def input():
-    return render_template('input.html')
+    if request.method == 'GET':
+        return render_template('input.html')
+    elif request.method == 'POST':
+        name = '乂煞氣a' + request.form['name'] + '乂'
+        id = request.form['id']
+        
+        return render_template('self_introduction.html',
+                               name=name,
+                               id=id)
 
 
 if __name__ == "__main__":
