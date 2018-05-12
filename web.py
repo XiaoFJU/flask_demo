34f4cb40204de8f62a52b2375b371e4a9ecbd820
xiao <freedom85812@gmail.com>
2018-05-12 21:10:07 +0800

    layout template  and not found page

--- web.py
+++ web.py
@@ -8,5 +8,10 @@ def hello():
     return 'Hello, World!'
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('status404.html', title="404"), 404
+
+
 if __name__ == "__main__":
     app.run()
