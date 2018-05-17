70b7fc1f3021c8d9086b6ed6550dfc1fb53bf388
xiao <freedom85812@gmail.com>
2018-05-17 15:56:27 +0800

    slides

--- web.py
+++ web.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, render_template_string
 
-app = Flask(__name__)
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='%%%%',
+        block_end_string='%%%%%>',
+        variable_start_string='%%%',
+        variable_end_string='%%%',
+        comment_start_string='%%%%%',
+        comment_end_string='%%%%%',
+    ))
+
+app = CustomFlask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, World!'
+    return render_template('slides.html')
 
 
 if __name__ == "__main__":
